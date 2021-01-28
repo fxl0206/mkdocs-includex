4584c7c5bd7eab95f3c433c3aedde517bae6ff81
liaoxf <liaoxf@asiainfo.com>
2021-01-28 16:12:02 +0800

    csv

--- mkdocs_includex/plugin.py
+++ mkdocs_includex/plugin.py
@@ -1,10 +1,36 @@
 import os 
 import csv
+import sys
+
 from jinja2 import Template
 from mkdocs import utils as mkdocs_utils
 from mkdocs.config import config_options
 from mkdocs.plugins import BasePlugin
  
+TPLX='\
+|    | animal_1   | animal_2   |\n\
+|---:|:-----------|:-----------|\n\
+|  0 | elk        | dog        |\n\
+|  1 | pig        | quetzal    |'
+
+def parseCsv(path):
+    cmd_str = ''
+    with open(path,encoding= 'gbk')as f:
+        f_csv = csv.reader(f)
+        line = 0
+        for row in f_csv :
+            curRow='|'
+            for col in row:
+                curRow=curRow+col+"|"
+            if line == 0 :
+                hsp='|'
+                for col in row:
+                    hsp=hsp+":---"+"|"
+                curRow=curRow+"\n"+hsp
+            cmd_str=cmd_str+"\n"+curRow
+            line=line+1
+        return cmd_str
+
 class IncludePlugin(BasePlugin):
 
     config_scheme = (
@@ -19,12 +45,7 @@ class IncludePlugin(BasePlugin):
         path = f'{self.config["src_path"]}/{filename}'
         suffix = os.path.splittext(path)[-1]
         if suffix == ".csv" :
-            cmd_str = ''
-            with open('test.csv')as f:
-                f_csv = csv.reader(f)
-                for row in f_csv:
-                    cmd_str=cmd_str+row
-                return cmd_str
+            return parseCsv(path)
         else:
             with open(path, 'r') as f:
                 return f.read()
@@ -33,3 +54,12 @@ class IncludePlugin(BasePlugin):
         self.page = page
         md_template = Template(markdown)
         return md_template.render(includex=self.includex)
+
+def main(argv):
+    path=os.path.splitext("1.txt")[-1]
+    print(path)
+    print(parseCsv("../test/hosts-info.csv"))
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
--- test/testx.py
+++ test/testx.py
@@ -1,9 +1,39 @@
 import os
 import sys
+import csv 
 
+TPLX='\
+|    | animal_1   | animal_2   |\n\
+|---:|:-----------|:-----------|\n\
+|  0 | elk        | dog        |\n\
+|  1 | pig        | quetzal    |'
+
+def parseCsv(path):
+    cmd_str = ''
+    with open(path,encoding= 'gbk')as f:
+        f_csv = csv.reader(f)
+        line = 0
+        for row in f_csv :
+            curRow='|'
+            for col in row:
+                curRow=curRow+col+"|"
+            if line == 0 :
+                hsp='|'
+                for col in row:
+                    hsp=hsp+":---"+"|"
+                curRow=curRow+"\n"+hsp
+            cmd_str=cmd_str+"\n"+curRow
+            line=line+1
+        return cmd_str
+#主机名|IP|端口|用户|密码
+#---|---|---|---|---
+#www.hubx.site|10.19.83.188|线下|archapp|线下
 def main(argv):
     path=os.path.splitext("1.txt")[-1]
     print(path)
+    print(parseCsv("./hosts-info.csv"))
+    print(TPLX)
+
 
 if __name__ == "__main__":
    main(sys.argv[1:])
